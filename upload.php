1b29819c12cc6d3377b4be776230dfcef39c8a06
WenTing <mybaby0103066@gmail.com>
2020-11-26 11:51:16 +0800

    新增上傳檔案的表單及基本處理程式

--- upload.php
+++ upload.php
@@ -6,6 +6,40 @@
  * 4.顯示檔案列表
  */
 
+
+date_default_timezone_set("Asia/Taipei");
+
+// $_FILES 專門用來處理檔案上傳
+if(!empty($_FILES['img']['tmp_name'])){
+    echo "檔案原始名稱:".$_FILES['img']['name'];
+    echo "<br>檔案上傳成功";
+    echo "原始上傳路徑:".$_FILES['img']['tmp_name'];
+
+    //方法一
+    //array_pop()可以將陣列的最後一位直接抓出來
+    $subname=(explode('.',$_FILES['img']['name']));
+    $subname=array_pop($subname);
+
+
+    //方法二
+    // $filename=date("Ymdhis");
+    // switch($_FILES['img']['type']){
+    //     case "image/jpeg":
+    //         $subname=".jpg";
+    //     break;
+    //     case "image/png":
+    //         $subname=".png";
+    //     break;
+    //     case "image/gif":
+    //         $subname=".gif";
+    //     break;
+    // }
+
+    $filename=date("Ymdhis").".".$subname;
+
+    move_uploaded_file($_FILES['img']['tmp_name'],"./img/".$filename);
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,9 +51,12 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
- <h1 class="header">檔案上傳練習</h1>
- <!----建立你的表單及設定編碼----->
-
+<h1 class="header">檔案上傳練習</h1>
+<!----建立你的表單及設定編碼----->
+<form action="?" method="post" enctype="multipart/form-data">  <!-- post可以傳送較大量的資料 -->
+    <input type="file" name="img">
+    <input type="submit" value="上傳">
+</form>
 
 
 
